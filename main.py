150697b011276a9116e3962822043bff919920f3
haresh <haresh2569@gmail.com>
2023-03-26 23:57:02 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#TODOS
+#crawl the keywords for tile and make the title
+    # get title from chat gpt wisper
+#create an about using the keywords by an GPT
+#generate the relevent tattoo from blackink
+#resize the image to pinterest requirments
+#create the pinterest pins
